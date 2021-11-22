089ec37bd12c6125cd8b86bbea60c4d4b1d034df
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-22 18:36:08 -0300

    Creating NextCloud start script.

---
+++ NextCloud/Start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+run_as_root
+
+# Reading authentication file
+set -a
+source auth.env
+
+# Updating NextCloud container
+docker-compose up -d
+
+# Starting NextCloud
+docker start my_nextcloud_db
+docker start my_nextcloud_app

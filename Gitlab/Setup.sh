41cbd3b510a7f28102c6b5ae876ff045f6821177
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-22 15:49:57 -0300

    Changing setup script to install / reinstall GitLab.

--- Gitlab/Setup.sh
+++ Gitlab/Setup.sh
@@ -3,5 +3,26 @@
 # Creating GitLab data folder
 sudo mkdir -p "/srv/gitlab/config:/etc/gitlab" "/srv/gitlab/logs:/var/log/gitlab" "/srv/gitlab/data:/var/opt/gitlab"
 
-# Running container
+# Stopping GitLab
+sudo docker stop my_gitlab
+
+# Removing old GitLab
+sudo docker rm my_gitlab
+
+# Updating GitLab container
 sudo docker-compose up -d
+
+# Starting GitLab
+sudo docker start my_gitlab
+
+# Updating GitLab
+sudo docker exec my_gitlab gitlab-ctl upgrade
+
+# Checking GitLab config
+sudo docker exec my_gitlab gitlab-ctl check-config
+
+# Starting GitLab
+sudo docker exec my_gitlab gitlab-ctl start
+sudo docker exec my_gitlab gitlab-ruby
+
+

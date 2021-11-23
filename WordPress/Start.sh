15b3950ca78b248cfd742128ad6ab304bb454e8a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-22 21:05:30 -0300

    Creating WordPress start script.

---
+++ WordPress/Start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+run_as_root
+
+# Reading authentication file
+set -a
+source auth.env
+
+# Updating WordPress container
+docker-compose up -d
+
+# Starting WordPress
+docker start my_wordpress_db
+docker start my_wordpress_app

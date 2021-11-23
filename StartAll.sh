be9ae517efba70aece85ce39c0df831bb8ee136c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-22 21:05:57 -0300

    Adding WordPress to StartAll.sh.

--- StartAll.sh
+++ StartAll.sh
@@ -6,7 +6,11 @@ bash Start.sh
 cd ".." || exit 1
 
 # Starting NextCloud
-cd "NextCloud" || exit
+cd "NextCloud" || exit 1
 bash Start.sh
 cd ".." || exit 1
 
+# Starting WordPress
+cd "WordPress" || exit 1
+bash Start.sh
+cd ".." || exit 1

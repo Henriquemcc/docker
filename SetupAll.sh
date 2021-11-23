d92974c095820807319f77e2db33f6928b94964f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-11-22 21:05:48 -0300

    Adding WordPress to SetupAll.sh.

--- SetupAll.sh
+++ SetupAll.sh
@@ -6,6 +6,11 @@ bash Setup.sh
 cd ".." || exit 1
 
 # Setting up NextCloud
-cd "NextCloud" || exit
+cd "NextCloud" || exit 1
+bash Setup.sh
+cd ".." || exit 1
+
+# Setting up WordPress
+cd "WordPress" || exit 1
 bash Setup.sh
 cd ".." || exit 1

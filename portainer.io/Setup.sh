3bd5e5817b682126c819a6b4bf369622a546f77e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-19 16:39:25 -0300

    Adding portainer.io container

---
+++ portainer.io/Setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Runs this script as root if it is not root.
+function run_as_root() {
+  if [ "$(whoami)" != "root" ]; then
+    echo "This script is not running as root"
+    if [ "$(command -v sudo)" ]; then
+      sudo bash "$0"
+      exit $?
+    else
+      echo "Sudo is not installed"
+      exit 1
+    fi
+  fi
+}
+
+# Running this script as root
+run_as_root
+
+# Deploying portainer
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes cr.portainer.io/portainer/agent:2.9.3
+
+# Adding port to firewall
+firewall-cmd --add-port=9443/tcp --permanent

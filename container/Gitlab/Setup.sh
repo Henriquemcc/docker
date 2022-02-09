2f4f5a805c4bc8b954ba13a6d43a5fe3ebff2f73
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-09 09:42:20 -0300

    Modifying script, so that it runs as root only if it can not run as the non-root user

--- container/Gitlab/Setup.sh
+++ container/Gitlab/Setup.sh
@@ -1,45 +1,28 @@
 #!/bin/bash
 
-# Runs this script as root if it is not root.
-function run_as_root() {
-  if [ "$(whoami)" != "root" ]; then
-    echo "This script is not running as root"
-    if [ "$(command -v sudo)" ]; then
-      sudo bash "$0"
-      exit $?
-    else
-      echo "Sudo is not installed"
-      exit 1
-    fi
-  fi
-}
-
-# Running this script as root
-run_as_root
-
 # Creating GitLab data folder
 mkdir -p "/srv/gitlab/config:/etc/gitlab" "/srv/gitlab/logs:/var/log/gitlab" "/srv/gitlab/data:/var/opt/gitlab"
 
 # Stopping GitLab
 docker stop my_gitlab
+sudo docker stop my_gitlab
 
 # Removing old GitLab
 docker rm my_gitlab
+sudo docker rm my_gitlab
 
 # Updating GitLab container
-docker-compose up -d
+docker-compose up -d || sudo docker-compose up -d
 
 # Starting GitLab
-docker start my_gitlab
+docker start my_gitlab || sudo docker start my_gitlab
 
 # Updating GitLab
-docker exec my_gitlab gitlab-ctl upgrade
+docker exec my_gitlab gitlab-ctl upgrade || sudo docker exec my_gitlab gitlab-ctl upgrade
 
 # Checking GitLab config
-docker exec my_gitlab gitlab-ctl check-config
+docker exec my_gitlab gitlab-ctl check-config || sudo docker exec my_gitlab gitlab-ctl check-config
 
 # Starting GitLab
-docker exec my_gitlab gitlab-ctl start
-docker exec my_gitlab gitlab-ruby
-
-
+docker exec my_gitlab gitlab-ctl start || sudo docker exec my_gitlab gitlab-ctl start
+docker exec my_gitlab gitlab-ruby || sudo docker exec my_gitlab gitlab-ruby

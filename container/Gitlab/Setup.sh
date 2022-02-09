78a912f28c4cef69af4003ef78e3cb5760212061
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-09 09:53:43 -0300

    Fixing error to create gitlab folders

--- container/Gitlab/Setup.sh
+++ container/Gitlab/Setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Creating GitLab data folder
-mkdir -p "/srv/gitlab/config:/etc/gitlab" "/srv/gitlab/logs:/var/log/gitlab" "/srv/gitlab/data:/var/opt/gitlab"
+etc_gitlab_dir="/srv/gitlab/config:/etc/gitlab"
+var_log_gitlab_dir="/srv/gitlab/logs:/var/log/gitlab"
+var_opt_gitlab_dir="/srv/gitlab/data:/var/opt/gitlab"
+mkdir -p "$etc_gitlab_dir" "$var_log_gitlab_dir" "$var_opt_gitlab_dir" || sudo mkdir -p "$etc_gitlab_dir" "$var_log_gitlab_dir" "$var_opt_gitlab_dir"
 
 # Updating GitLab container
 docker-compose up -d || sudo docker-compose up -d

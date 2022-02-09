0abe2d426e6d6c5ac3899751a804589c32c4d465
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-09 09:42:25 -0300

    Modifying script, so that it runs as root only if it can not run as the non-root user

--- container/Gitlab/Start.sh
+++ container/Gitlab/Start.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-# Runs this script as root if it is not root.
-function run_as_root() {
-  if [ "$(whoami)" != "root" ]; then
-    echo "This script is not running as root"
-    if [ "$(command -v sudo)" ]; then
-      sudo bash "$0"
-      exit $?
-    else
-      echo "Sudo is not installed"
-      exit 1
-    fi
-  fi
-}
-
-# Running this script as root
-run_as_root
-
-# Updating GitLab container
-docker-compose up -d
-
 # Starting GitLab
-docker start my_gitlab
+docker start my_gitlab || sudo docker start my_gitlab
 
 # Updating gitlab
-docker exec my_gitlab gitlab-ctl upgrade
+docker exec my_gitlab gitlab-ctl upgrade || sudo docker exec my_gitlab gitlab-ctl upgrade
 
 # Checking GitLab config
-docker exec my_gitlab gitlab-ctl check-config
+docker exec my_gitlab gitlab-ctl check-config || sudo docker exec my_gitlab gitlab-ctl check-config
 
 # Starting GitLab
-docker exec my_gitlab gitlab-ctl start
-docker exec my_gitlab gitlab-ruby
+docker exec my_gitlab gitlab-ctl start || sudo docker exec my_gitlab gitlab-ctl start
+docker exec my_gitlab gitlab-ruby || sudo docker exec my_gitlab gitlab-ruby

6b427068cfda62f7e01bc8339410f71c82d04e3d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-09 10:00:41 -0300

    Modifying script, so that it runs as root only if it can not run as the non-root user

--- container/NextCloud/Start.sh
+++ container/NextCloud/Start.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
 
-# Runs this script as root if it is not root.
-function run_as_root() {
-  if [ "$(whoami)" != "root" ]; then
-    echo "This script is not running as root"
-    if [ "$(command -v sudo)" ]; then
-      sudo bash "$0"
-      exit $?
-    else
-      echo "Sudo is not installed"
-      exit 1
-    fi
-  fi
-}
-
-# Running this script as root
-run_as_root
-
-# Reading authentication file
-set -a
-source auth.env
-
-# Building container
-docker-compose build --pull
-
-# Updating NextCloud container
-docker-compose up -d
-
-# Starting NextCloud
-docker start my_nextcloud_db
-docker start my_nextcloud_app
+docker start my_nextcloud_db || sudo docker start my_nextcloud_db
+docker start my_nextcloud_app || sudo docker start my_nextcloud_app

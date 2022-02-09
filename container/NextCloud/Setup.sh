09875aa16d62eace0817c134ec4a907b6e56125e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-02-09 10:01:22 -0300

    Removing the stop, remove and start parts from the setup script

--- container/NextCloud/Setup.sh
+++ container/NextCloud/Setup.sh
@@ -5,14 +5,6 @@ app_dir="/srv/nextcloud/app"
 db_dir="/srv/nextcloud/db"
 mkdir -p "$app_dir" "$db_dir" || sudo mkdir -p "$app_dir" "$db_dir"
 
-# Stopping NextCloud
-docker stop my_nextcloud_app || sudo docker stop my_nextcloud_app
-docker stop my_nextcloud_db || sudo docker stop my_nextcloud_db
-
-# Removing old NextCloud
-docker rm my_nextcloud_app || sudo docker rm my_nextcloud_app
-docker rm my_nextcloud_db || sudo docker rm my_nextcloud_db
-
 # Reading authentication file
 set -a
 source auth.env
@@ -21,8 +13,4 @@ source auth.env
 docker-compose build --pull || sudo docker-compose build --pull
 
 # Updating NextCloud container
-docker-compose up -d
-
-# Starting NextCloud
-docker start my_nextcloud_app
-docker start my_nextcloud_db
+docker-compose up -d || sudo docker-compose up -d
